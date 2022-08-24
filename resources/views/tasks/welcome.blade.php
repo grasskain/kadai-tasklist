60b3ddfaf976a65155b52c50ad3520a1d2de9f33
moeka.imamura <m.imamura0274@gmail.com>
2022-08-24 02:03:29 +0000

    user registration

--- resources/views/tasks/welcome.blade.php
+++ resources/views/tasks/welcome.blade.php
@@ -97,4 +97,15 @@
             </div>
         </div>
     </body>
+    @extends('layouts.app')
+
+    @section('content')
+    <div class="center jumbotron">
+        <div class="text-center">
+            <h1>Welcome to the Microposts</h1>
+            {{-- ユーザ登録ページへのリンク --}}
+            {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+        </div>
+    </div>
+    @endsection
 </html>
