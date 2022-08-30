4a1e17a56c60e0fc1806e8e7ba9a3622c0df7e69
moeka.imamura <m.imamura0274@gmail.com>
2022-08-30 06:15:12 +0000

    Procfile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 
         <div class="center jumbotron">
             <div class="text-center">
-                <h1>Welcome to the tasklis</h1>
+                <h1>Welcome to the tasklist</h1>
                 
                     {{-- ユーザ登録ページへのリンク --}}
                     {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
