e84c73b94522a924dd0f3296d658a1b69d6b6498
moeka.imamura <m.imamura0274@gmail.com>
2022-08-30 05:48:25 +0000

    kadai-tasklist

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,12 @@
- @extends('layouts.app')
+@extends('layouts.app')
 
 @section('content')
 
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the tasklist</h1>
-                       {{-- ユーザ登録ページへのリンク --}}
-                       {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
+                    {{-- ユーザ登録ページへのリンク --}}
+                    {!! link_to_route('signup.get', 'Sign up now!', [], ['class' => 'btn btn-lg btn-primary']) !!}
             </div>
         </div>
           
